44bed1108b4ad0ef8adb3683411f2b648922c7ab
JesusVasari <jlopez 2rafaelalberti.es>
2021-11-30 18:27:25 +0100

    some funs work

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,31 +1,43 @@
-class Pila<T>(t: T) {
-    
-        var lista = mutableListOf<T>()
+class Pila<T>() {
+
+    private var listaPila = mutableListOf<T>()
 
     fun tope(): Int {
-        return lista.lastIndex;(lista)
-
+        return listaPila.lastIndex
     }
 
-    fun push() {
-       // lista.add()
-
-
+    fun push(element: T)  {
+        listaPila.add(element)
     }
 
-    fun pop() {
-    //    lista.remove()
+    fun pop(element : T) {
+        listaPila.remove(element)
     }
 
     fun vacia() {
 
 
     }
-
+    fun reverse(listaPila: MutableList<T>) {
+        var alreves = reverse(this.listaPila)
+        if (!listaPila.equals(alreves)){
+            println("error")
+        }else{
+            println("correcto")
+        }
+    }
 }
+
+
+
 
 fun main(args: Array<String>) {
 
-
-
+    var plato1 = Pila<Any>()
+    var plato2 = Pila<Any>()
+    var plato3 = Pila<Any>()
+    var plato5 = Pila<Any>()
+    var plato6 = Pila<Any>()
+    var plato7 = Pila<Any>()
+    var plato8 = Pila<Any>()
 }
